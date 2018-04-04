04c2b58ec558dffebc929ca5b01ce3d2a1a82db1
Salvador Guzman <guzmansalv@gmail.com>
2018-04-04 03:58:04 -0700

    an input for user, repo, branch and file

--- brotlifrontend/src/com/github/sguzman/brotli/frontend/Main.scala
+++ brotlifrontend/src/com/github/sguzman/brotli/frontend/Main.scala
@@ -3,7 +3,7 @@ package com.github.sguzman.brotli.frontend
 import com.thoughtworks.binding.Binding.Var
 import com.thoughtworks.binding.{Binding, dom}
 import org.scalajs.dom.{Event, document}
-import org.scalajs.dom.html.{Div, Input}
+import org.scalajs.dom.html.{Button, Div, Input}
 import org.scalajs.dom.raw.Element
 
 import scala.language.reflectiveCalls
@@ -13,8 +13,8 @@ object Main {
     def id[A] = document.getElementById(str).asInstanceOf[A]
   }
 
-  final case class Model(user: Var[String], repo: Var[String])
-  val model = Model(Var(""), Var(""))
+  final case class Model(user: Var[String], repo: Var[String], branch: Var[String], file: Var[String], brotli: Var[Boolean])
+  val model = Model(Var(""), Var(""), Var(""), Var(""), Var(false))
 
   sealed abstract class UpdateType(e: Event) {
     def `type` = e.`type`
@@ -22,7 +22,9 @@ object Main {
 
   final case class onUserInput(e: Event) extends UpdateType(e)
   final case class onRepoInput(e: Event) extends UpdateType(e)
-  final case class onNil(e: Event) extends UpdateType(e)
+  final case class onBranchInput(e: Event) extends UpdateType(e)
+  final case class onFileInput(e: Event) extends UpdateType(e)
+  final case class onButtonClick(e: Event) extends UpdateType(e)
 
   def update(msg: UpdateType): Unit = {
     msg match {
@@ -30,22 +32,34 @@ object Main {
         model.user.value = "user".id[Input].value
       case _: onRepoInput =>
         model.repo.value = "repo".id[Input].value
-      case _: onNil => {}
+      case _: onBranchInput =>
+        model.branch.value = "branch".id[Input].value
+      case _: onFileInput =>
+        model.file.value = "file".id[Input].value
+      case _: onButtonClick => println("clicked me!")
     }
   }
 
   def emit[A <: Element](e: Event): Unit = {
     val target = e.currentTarget.asInstanceOf[A].id
-    val typ = e.`type`
+    val t = e.`type`
 
     val msg = target match {
-      case "repo" => typ match {
+      case "repo" => t match {
         case "input" => onRepoInput(e)
       }
-      case "user" => typ match {
+      case "user" => t match {
         case "input" => onUserInput(e)
       }
-      case _ => onNil(e)
+      case "branch" => t match {
+        case "input" => onBranchInput(e)
+      }
+      case "file" => t match {
+        case "input" => onFileInput(e)
+      }
+      case "button" => t match {
+        case "click" => onButtonClick(e)
+      }
     }
 
     update(msg)
@@ -53,11 +67,26 @@ object Main {
 
   @dom def view(model: Model): Binding[Div] = {
     <div>
-      <h1>Hello</h1>
-      <input id="user" oninput={emit[Input] _}></input>
-      <input id="repo" oninput={emit[Input] _}></input>
+      <h1>Add Brotli Compression encoding</h1>
+      <h2>How it works</h2>
+      <div>
+        <p>Add the user, repo, branch and file you plan on linking to</p>
+        <p>If you plan on using Brotli encoding, don't forget to set the checkmark</p>
+        <input id="user" oninput={emit[Input] _}></input>
+        <input id="repo" oninput={emit[Input] _}></input>
+        <input id="branch" oninput={emit[Input] _}></input>
+        <input id="file" oninput={emit[Input] _}></input>
+        <input type="checkbox" id="brotli" oninput={emit[Input] _}></input>
+        <button id="button" onclick={emit[Button] _}></button>
+      </div>
       <p>User: {model.user.bind}</p>
       <p>Repo: {model.repo.bind}</p>
+      <p>Branch: {model.branch.bind}</p>
+      <p>File: {model.file.bind}</p>
+      <p></p>
+      <div>
+        Now you can use the generated path in your HTML
+      </div>
     </div>
   }
 
